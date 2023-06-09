c79f59dd1eea2b636b775cb361e84b8031c02846
shanu2409 <shanusingh2409@gmail.com>
2023-06-10 04:55:16 +0530

    done

---
+++ src/hooks/useCategory.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useCategory() {
+  const [categories, setCategories] = useState([]);
+
+  //get cat
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://shopitbackend.onrender.com" + "/api/v1/category/get-category"
+      );
+      setCategories(data?.category);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+  return categories;
+}
